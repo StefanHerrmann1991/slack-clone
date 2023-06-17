1e3c71fe4c79ec464ff96b800a150711f7f03c00
StefanHerrmann1991 <sth1812@posteo.de>
2023-06-17 09:18:06 +0200

    blant version with firestore and angular material

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'slack-clone-f581f',
+    appId: '1:545470931164:web:50a49ec350860aa91d9419',
+    storageBucket: 'slack-clone-f581f.appspot.com',
+    apiKey: 'AIzaSyAJa-BhxYTMf22uISwPymEX-fJp7bXfxSQ',
+    authDomain: 'slack-clone-f581f.firebaseapp.com',
+    messagingSenderId: '545470931164',
+  },
+};
