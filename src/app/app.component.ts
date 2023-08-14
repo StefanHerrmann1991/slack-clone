6a3b94cac7af4ac3bc7ceb06a39cc58a9795c8cd
StefanHerrmann1991 <sth1812@posteo.de>
2023-08-14 02:18:35 +0200

    working theming background in angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,16 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'slack-clone';
-  isDarkTheme = false;
+  private isDarkTheme = true;
 
-  toggleTheme() {
-      this.isDarkTheme = !this.isDarkTheme;
-      if (this.isDarkTheme) {
-          document.body.classList.add('dark-theme');
-          document.body.classList.remove('light-theme');
-      } else {
-          document.body.classList.add('light-theme');
-          document.body.classList.remove('dark-theme');
-      }
+  @HostBinding('class')
+  get themeMode() {
+    return this.isDarkTheme ? 'theme-dark' : 'theme-light';
   }
 }
